ff7ecd001c622ec6a9bef117e2b444388f5552e0
Talal Adnan <Talal.A.Adnan@bbc.co.uk>
2026-07-14 14:37:18 +0100

    database.js

---
+++ database.js
@@ -0,0 +1,45 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+async function connectDB() {
+    return open({
+        filename: "./test.db",
+        driver: sqlite3.Database
+    });
+}
+
+async function submitOTJ(db, dateString, userID, hours, description) {
+    await db.run(
+        "INSERT INTO otj (date, userId, hours, description)"
+        + `VALUES ('${dateString}', '${userID}', '${hours}', '${description}');`
+    );
+}
+
+async function removeOTJ(db, otjID) {
+    await db.run(`DELETE FROM otj WHERE otjID = ${otjID};`);
+}
+
+async function addUser(db, name, courseID, courseStartDate) {
+    db.run(
+        "INSERT INTO users (name, courseID, courseStartDate)"
+        + `VALUES ('${name}', '${courseID}' '${courseStartDate}');`
+    )
+}
+
+async function deleteUser(db, userID) {
+    db.run(`DELETE FROM users WHERE userID = ${userID};`);
+}
+
+async function main() {
+    const db = await connectDB();
+
+    const users = await db.all(
+        "SELECT * FROM users"
+    );
+
+    console.log(users);
+}
+
+
+main();
+
