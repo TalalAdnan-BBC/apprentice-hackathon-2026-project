ebe876125d87b52f0a219cb89a14cfe84cb4e518
frocket-exe <freddie.rockett@gmail.com>
2026-07-14 16:04:14 +0100

    Export any user OTJ Log to CSV

--- otj-export.js
+++ otj-export.js
@@ -1,5 +1,6 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
+const fs = require("fs");
 
 
 async function connectDB() {
@@ -10,16 +11,46 @@ async function connectDB() {
 }
 
 
-async function main() {
+async function ksbList(otjID) {
     const db = await connectDB();
 
-    const users = await db.all(
-        "SELECT date, hours, description FROM otj WHERE userID = 1 ORDER BY date ASC"
+    const ksbLogs = await db.all(
+        `SELECT ksb FROM ksb WHERE otjID = ${otjID}`
     );
 
-    console.log(users);
+    let ksbList = "";
+    ksbLogs.forEach(ksb => {
+        ksbList += `${ksb.ksb}, `
+    });
+    ksbList = ksbList.slice(0, -2);
+
+    return(ksbList);
 }
 
 
-main();
+async function getCSV(userID) {
+    const db = await connectDB();
 
+    const otjLogs = await db.all(
+        `SELECT date, hours, description, otjID FROM otj WHERE userID = ${userID} ORDER BY date ASC`
+    );
+
+    console.log(otjLogs);
+
+    let csv = "Date,Description,KSBs,Hours\n";
+
+    for (const log of otjLogs) {
+        const ksbString = await ksbList(log.otjID);
+        csv += `"${log.date}","${log.description}","${ksbString}","${log.hours}"\n`;
+    }
+
+    fs.writeFileSync("otjLogs.csv", csv);
+}
+
+
+async function getKSB(otjID)
+{
+    console.log(await ksbList(otjID));
+}
+
+getCSV(1);
