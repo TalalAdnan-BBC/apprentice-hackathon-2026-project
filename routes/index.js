eaeafbbdc050c30645afee7549d69396a4360ca8
graceAlagaratnam <grace.alagaratnam@bbc.co.uk>
2026-07-14 14:13:45 +0100

    fix: added pug

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
+const app = express(); //module.exports = express.createServer;
 const port = 3000;
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
